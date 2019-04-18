8a67c0a18204175c95f62f3495ce4ebe2f0da4cb
bulang <1107842278@qq.com>
2019-04-18 18:11:39 +0800

    修改路由引用

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const koa = require("koa");
 const router = require("./sha/route");
 const baseCfg = require("./config/base");
 const app = new koa();
-app.use(router.routes(),router.allowedMethods());
+app.use(router().routes(),router().allowedMethods());
 app.listen(baseCfg.POART);
 console.log(app);
 console.log("server listening on http://127.0.0.1:"+baseCfg.POART);
